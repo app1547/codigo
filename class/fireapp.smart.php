28e42a281948b05370c6492db063989cd0e0651b
diegomez13 <diegomez13@hotmail.com>
2016-03-14 17:42:59 -0300

    vv

--- class/fireapp.smart.php
+++ class/fireapp.smart.php
@@ -95,31 +95,31 @@ class FireappSmart extends Fireapp{
     
     public function getLlamado(){
         
-        $info = $this->token();
         $this->status();
+        $id_act = $this->getpost('idact');
+        if(is_int($id_act)){
+            
+            $llamado = $this->con->sql("SELECT * FROM actos t1, claves t2 WHERE t1.id_act='".$id_act."' AND t1.id_cla=t2.id_cla");
+        
+            $aux['clave'] = $llamado['resultado'][0]['nombre'];
+            $aux['direccion'] = $llamado['resultado'][0]['direccion'];
+            $aux['maquinas'] = $llamado['resultado'][0]['maquinas'];
+            $aux['lat'] = $llamado['resultado'][0]['lat'];
+            $aux['lng'] = $llamado['resultado'][0]['lng'];
+            $aux['datetime'] = $llamado['resultado'][0]['fecha'];
+            $aux['preinforme'] = $llamado['resultado'][0]['preinforme'];
+            
+            //$aux['acargocuerpo'] = "DIEGO GOMEZ B 13 cia"; // FUNCION QUE MUESTRA EL QUE ESTA A CARGO DEL CUERPO
+            
+            $maquinas = $this->con->sql("SELECT * FROM actos_maquina t1, maquinas t2, usuarios t3 WHERE t1.id_act='".$id_act."' AND t1.id_maq=t2.id_maq AND t1.id_user=t3.id_user");
+            print_r($maquinas);
+            
+            $aux2['nombre'] = "B13";
+            $aux2['acargo'] = "Diego Gomez B.";
+            $aux2['6'] = "6";
+            $aux2['lat'] = "-33.439797";
+            $aux2['lng'] = "-70.616939";
 
-        if($info['estado']){
-            
-            // SI ES USER
-            $id_user = $info['id_user'];
-            $id_cia = $info['id_cia'];
-            $id_cue = $info['id_cue'];
-            
-            $aux['clave'] = "10-0-1";
-            $aux['direccion'] = "Jose Tomas Rider 1185";
-            $aux['maquinas'] = "B13 B14 Q15";
-            $aux['acargocuerpo'] = "DIEGO GOMEZ B 13 cia"; // FUNCION QUE MUESTRA EL QUE ESTA A CARGO DEL CUERPO
-            $aux['lat'] = "-33.439797";
-            $aux['lng'] = "-70.616939";
-            $aux['datetime'] = "27-09-1984 18:30";
-            $aux['preinforme'] = "Se trata de fuego en cocina, se trabaja";
-            
-            $aux2['nombre'] = "B13";
-            $aux2['acargo'] = "Diego Gomez B.";
-            $aux2['6'] = "6";
-            $aux2['lat'] = "-33.439797";
-            $aux2['lng'] = "-70.616939";
-            
             $aux['lmaquinas'][] = $aux2;
 
             $aux2['nombre'] = "B13";
@@ -127,61 +127,56 @@ class FireappSmart extends Fireapp{
             $aux2['6'] = "6";
             $aux2['lat'] = "-33.439797";
             $aux2['lng'] = "-70.616939";
-            
+
             $aux['lmaquinas'][] = $aux2;
-            
+
             $aux2['nombre'] = "B13";
             $aux2['acargo'] = "Diego Gomez B.";
             $aux2['6'] = "6";
             $aux2['lat'] = "-33.439797";
             $aux2['lng'] = "-70.616939";
-            
+
             $aux['lmaquinas'][] = $aux2;
-            
+
             $aux['grifos'][0]['lat'] = "-33.439797";
             $aux['grifos'][0]['lng'] = "-70.616939";
             $aux['grifos'][0]['metros'] = "35";
-            
+
             $aux['grifos'][1]['lat'] = "-33.437797";
             $aux['grifos'][1]['lng'] = "-70.612939";
             $aux['grifos'][1]['metros'] = "245";
-            
+
             $aux['grifos'][2]['lat'] = "-33.435797";
             $aux['grifos'][2]['lng'] = "-70.611939";
             $aux['grifos'][2]['metros'] = "785";
-            
-            
+
+
             $aux['volcamino'][0]['nombre'] = "Diego Gomez";
             $aux['volcamino'][0]['lat'] = "-33.439797";
             $aux['volcamino'][0]['lng'] = "-70.612939";
             $aux['volcamino'][0]['metros'] = "2100";
             $aux['volcamino'][0]['segundos'] = "180";
             $aux['volcamino'][1]['cia'] = "13";
-            
+
             $aux['volcamino'][1]['nombre'] = "Juan Perez";
             $aux['volcamino'][1]['lat'] = "-33.439197";
             $aux['volcamino'][1]['lng'] = "-70.614939";
             $aux['volcamino'][1]['metros'] = "1700";
             $aux['volcamino'][1]['segundos'] = "130";
             $aux['volcamino'][1]['cia'] = "14";
-            
+
             $aux['volcamino'][2]['nombre'] = "Jorge Diaz";
             $aux['volcamino'][2]['lat'] = "-33.439297";
             $aux['volcamino'][2]['lng'] = "-70.614239";
             $aux['volcamino'][2]['metros'] = "1950";
             $aux['volcamino'][2]['segundos'] = "145";
             $aux['volcamino'][2]['cia'] = "15";
-            
-            
+
+
             $this->anexostatus("llamados", $aux);
             $this->setstatus(1, "Info Llamado");
-            
-        }else{
-            
-            // SI NO ES USER
-            $this->setstatus(0, "No se reconoce Usuario");
-            
         }
+        
         return $this->getstatus();
         
     }
