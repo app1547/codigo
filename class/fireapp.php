a2570c7506754d8471f59dc46735a9721d118675
diegomez13 <diegomez13@hotmail.com>
2016-03-14 14:54:51 -0300

    aa

--- class/fireapp.php
+++ class/fireapp.php
@@ -4,9 +4,25 @@ include('mysql_class.php');
 
 class Fireapp{
     
+    public $status = null;
+    
     public function __construct(){
             
     }
-    
+    public function status(){
+        // 0 = ERROR, 1 = OK
+        $this->status['estado'] = 0;
+        $this->status['mensaje'] = "";
+    }
+    public function setstatus($estado, $mensaje){
+        $this->status['estado'] = $estado;
+        $this->status['mensaje'] = $mensaje;
+    }
+    public function anexostatus($key, $mensaje){
+        $this->status[$key] = $mensaje;
+    }
+    public function getstatus(){
+        return $this->status;
+    }
 }
 ?>
