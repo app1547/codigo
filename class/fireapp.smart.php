6a512f01ede2a3137a8762a631fbee4959011138
diegomez13 <diegomez13@hotmail.com>
2016-03-15 17:59:12 -0300

    aa

--- class/fireapp.smart.php
+++ class/fireapp.smart.php
@@ -210,8 +210,6 @@ class FireappSmart extends Fireapp{
                     $act = $this->con->sql("SELECT lat, lng FROM actos WHERE id_act='".$id_act."'");
                     $google = $this->getgoogledist($act['resultado'][0]['lat'], $lat, $act['resultado'][0]['lng'], $lng, $modo);
                     $s = $this->con->sql("UPDATE actos_user_camino SET lat='".$lat."', lng='".$lng."', lat_actual='".$lat."', lng_actual='".$lng."', modo='".$modo."', fecha=now(), posicion='1', distancia='".$google['distvalue']."', tiempo='".$google['timevalue']."' WHERE id_act='".$id_act."' AND id_user='".$id_user."'");
-                    echo "1<br><br>";
-                    print_r($s);
                 }
                 
             }else{
@@ -221,11 +219,9 @@ class FireappSmart extends Fireapp{
                         $act = $this->con->sql("SELECT lat, lng FROM actos WHERE id_act='".$id_act."'");
                         $google = $this->getgoogledist($act['resultado'][0]['lat'], $lat, $act['resultado'][0]['lng'], $lng, $modo);
                         $this->con->sql("UPDATE actos_user_camino SET lat='".$lat."', lng='".$lng."', lat_actual='".$lat."', lng_actual='".$lng."', modo='".$modo."', fecha=now(), posicion='1', distancia='".$google['distvalue']."', tiempo='".$google['timevalue']."' WHERE id_act='".$id_act."' AND id_user='".$id_user."'");
-                        echo "2";
                     }else{
                         $s = $this->con->sql("UPDATE actos_user_camino SET lat_actual='".$lat."', lng_actual='".$lng."' WHERE id_act='".$id_act."' AND id_user='".$id_user."'");
-                        echo "3<br/><br/>";
-                        print_r($s);
+                        $this->con->sql("INSERT INTO history (id_user, lat, lng) VALUES ('".$id_user."', '".$lat."', '".$lng."')");
                     }
                 }
                 
