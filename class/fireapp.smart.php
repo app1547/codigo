89122067905c17b7e9ed82e28bf6e1a2a0d3dc72
diegomez13 <diegomez13@hotmail.com>
2016-03-15 17:23:33 -0300

    ff

--- class/fireapp.smart.php
+++ class/fireapp.smart.php
@@ -223,7 +223,7 @@ class FireappSmart extends Fireapp{
                         $this->con->sql("UPDATE actos_user_camino SET lat='".$lat."', lng='".$lng."', lat_actual='".$lat."', lng_actual='".$lng."', modo='".$modo."', fecha=now(), posicion='1', distancia='".$google['distvalue']."', tiempo='".$google['timevalue']."' WHERE id_act='".$id_act."' AND id_user='".$id_user."'");
                         echo "2";
                     }else{
-                        $s = $this->con->sql("UPDATE actos_user_camino SET lat_actual='".$lat."' AND lng_actual='".$lng."' WHERE id_act='".$id_act."' AND id_user='".$id_user."'");
+                        $s = $this->con->sql("UPDATE actos_user_camino SET lat_actual='".$lat."', lng_actual='".$lng."' WHERE id_act='".$id_act."' AND id_user='".$id_user."'");
                         echo "3<br/><br/>";
                         print_r($s);
                     }
